f661cb9c96487c5236f5bfb3ed3c989cea71e8f5
shahadalmouh <47005936+shahadalmouh@users.noreply.github.com>
2019-02-18 21:33:25 +0300

    p

---
+++ hubot/example1.js
@@ -0,0 +1,88 @@
+'use strict'
+
+// Description:
+//   Example scripts for you to examine and try out.
+//
+// Notes:
+//   They are commented out by default, because most of them are pretty silly and
+//   wouldn't be useful and amusing enough for day to day huboting.
+//   Uncomment the ones you want to try and experiment with.
+//
+//   These are from the scripting documentation: https://github.com/github/hubot/blob/master/docs/scripting.md
+
+module.exports = (robot) => {
+
+
+
+  robot.hear(/hello/i, (res) => {
+    return res.send("hello, What type of food do you like?")
+     
+  })
+
+  robot.respond(/food type (.*)/i, (res) => {
+    const foodType = res.match[1]
+    
+    
+  
+    if (foodType === 'italian') {
+      res.reply('nino restaueant')
+      return
+    }
+
+    if (foodType === 'japanese') {
+      res.reply('sushiyoshi restaueant ')
+      return
+    }
+    
+    res.reply('try again')
+  })
+
+  robot.respond(/do(.*)/i, (res) => {
+    const foodType = res.match[1]
+    
+    
+  
+    if (foodType === '+') {
+      let x=3+3
+      res.send(x)
+      return
+    }
+
+    if (foodType === '*') {
+      let y=3*3
+      res.reply(y)
+      return
+    }
+    
+    
+  })
+
+
+  
+  const odd = ['1', '3', '5', '7', '9']
+  const even = ['2', '4', '6', '8', '10']
+  
+  robot.respond(`/${odd.join('|')}/i`, (res) => {
+    
+    res.send(res.random(odd))
+  })
+
+  robot.respond(`/${even.join('|')}/i`, (res) => {
+    
+    res.send(res.random(even))
+  })
+
+
+  robot.respond(/I don t like Sam-I-am/i, (res) => {
+    room =  '@shahadalmouh'
+    robot.messageRoom (room,"Someone does not like Dr. Seus" )
+    res.reply(`try again`)
+  })
+
+  setTimeout(() => {
+    res.send("is there any one'?", 60 * 1000)
+  }, timeout);
+  
+}
+
+
